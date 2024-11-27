537ea175e11a71580961565b31f2e3fa4f142042
fanavarro <francisco.abad@um.es>
2024-11-27 17:07:32 +0100

    Use new IRI for issues.

--- src/main/java/es/um/dis/tecnomod/huron/issues/IssueTypes.java
+++ src/main/java/es/um/dis/tecnomod/huron/issues/IssueTypes.java
@@ -1,49 +1,51 @@
 package es.um.dis.tecnomod.huron.issues;
 
+import es.um.dis.tecnomod.oquo.utils.Namespaces;
+
 /**
  * The Class IssueTypes.
  */
 public class IssueTypes {
 	
 	/** The Constant ANNOTATION_PROPERTY_WITH_NO_DESCRIPTION_ISSUE. */
-	public static final String ANNOTATION_PROPERTY_WITH_NO_DESCRIPTION_ISSUE = "https://purl.archive.org/oquo#AnnotationPropertyWithNoDescriptionIssue";
+	public static final String ANNOTATION_PROPERTY_WITH_NO_DESCRIPTION_ISSUE = Namespaces.OQUO_NS + "AnnotationPropertyWithNoDescriptionIssue";
 	
 	/** The Constant CLASS_WITH_NO_DESCRIPTION_ISSUE. */
-	public static final String CLASS_WITH_NO_DESCRIPTION_ISSUE = "https://purl.archive.org/oquo#ClassWithNoDescriptionIssue";
+	public static final String CLASS_WITH_NO_DESCRIPTION_ISSUE = Namespaces.OQUO_NS + "ClassWithNoDescriptionIssue";
 	
 	/** The Constant DATA_PROPERTY_WITH_NO_DESCRIPTION_ISSUE. */
-	public static final String DATA_PROPERTY_WITH_NO_DESCRIPTION_ISSUE = "https://purl.archive.org/oquo#DataPropertyWithNoDescriptionIssue";
+	public static final String DATA_PROPERTY_WITH_NO_DESCRIPTION_ISSUE = Namespaces.OQUO_NS + "DataPropertyWithNoDescriptionIssue";
 	
 	/** The Constant OBJECT_PROPERTY_WITH_NO_DESCRIPTION_ISSUE. */
-	public static final String OBJECT_PROPERTY_WITH_NO_DESCRIPTION_ISSUE = "https://purl.archive.org/oquo#ObjectPropertyWithNoDescriptionIssue";
+	public static final String OBJECT_PROPERTY_WITH_NO_DESCRIPTION_ISSUE = Namespaces.OQUO_NS + "ObjectPropertyWithNoDescriptionIssue";
 
 	/** The Constant ANNOTATION_PROPERTY_WITH_NO_NAME_ISSUE. */
-	public static final String ANNOTATION_PROPERTY_WITH_NO_NAME_ISSUE = "https://purl.archive.org/oquo#AnnotationPropertyWithNoNameIssue";
+	public static final String ANNOTATION_PROPERTY_WITH_NO_NAME_ISSUE = Namespaces.OQUO_NS + "AnnotationPropertyWithNoNameIssue";
 	
 	/** The Constant CLASS_WITH_NO_NAME_ISSUE. */
-	public static final String CLASS_WITH_NO_NAME_ISSUE = "https://purl.archive.org/oquo#ClassWithNoNameIssue";
+	public static final String CLASS_WITH_NO_NAME_ISSUE = Namespaces.OQUO_NS + "ClassWithNoNameIssue";
 	
 	/** The Constant DATA_PROPERTY_WITH_NO_NAME_ISSUE. */
-	public static final String DATA_PROPERTY_WITH_NO_NAME_ISSUE = "https://purl.archive.org/oquo#DataPropertyWithNoNameIssue";
+	public static final String DATA_PROPERTY_WITH_NO_NAME_ISSUE = Namespaces.OQUO_NS + "DataPropertyWithNoNameIssue";
 	
 	/** The Constant OBJECT_PROPERTY_WITH_NO_NAME_ISSUE. */
-	public static final String OBJECT_PROPERTY_WITH_NO_NAME_ISSUE = "https://purl.archive.org/oquo#ObjectPropertyWithNoNameIssue";
+	public static final String OBJECT_PROPERTY_WITH_NO_NAME_ISSUE = Namespaces.OQUO_NS + "ObjectPropertyWithNoNameIssue";
 
 	/** The Constant ANNOTATION_PROPERTY_WITH_NO_SYNONYMS_ISSUE. */
-	public static final String ANNOTATION_PROPERTY_WITH_NO_SYNONYMS_ISSUE = "https://purl.archive.org/oquo#AnnotationPropertyWithNoSynonymsIssue";
+	public static final String ANNOTATION_PROPERTY_WITH_NO_SYNONYMS_ISSUE = Namespaces.OQUO_NS + "AnnotationPropertyWithNoSynonymsIssue";
 	
 	/** The Constant CLASS_WITH_NO_SYNONYMS_ISSUE. */
-	public static final String CLASS_WITH_NO_SYNONYMS_ISSUE = "https://purl.archive.org/oquo#ClassWithNoSynonymsIssue";
+	public static final String CLASS_WITH_NO_SYNONYMS_ISSUE = Namespaces.OQUO_NS + "ClassWithNoSynonymsIssue";
 	
 	/** The Constant DATA_PROPERTY_WITH_NO_SYNONYMS_ISSUE. */
-	public static final String DATA_PROPERTY_WITH_NO_SYNONYMS_ISSUE = "https://purl.archive.org/oquo#DataPropertyWithNoSynonymsIssue";
+	public static final String DATA_PROPERTY_WITH_NO_SYNONYMS_ISSUE = Namespaces.OQUO_NS + "DataPropertyWithNoSynonymsIssue";
 	
 	/** The Constant OBJECT_PROPERTY_WITH_NO_SYNONYMS_ISSUE. */
-	public static final String OBJECT_PROPERTY_WITH_NO_SYNONYMS_ISSUE = "https://purl.archive.org/oquo#ObjectPropertyWithNoSynonymsIssue";
+	public static final String OBJECT_PROPERTY_WITH_NO_SYNONYMS_ISSUE = Namespaces.OQUO_NS + "ObjectPropertyWithNoSynonymsIssue";
 
 	/** The Constant LEXICALLY_SUGGEST_LOGICALLY_DEFINE_ISSUE. */
-	public static final String LEXICALLY_SUGGEST_LOGICALLY_DEFINE_ISSUE = "https://purl.archive.org/oquo#LexicallySuggestLogicallyDefineIssue";
+	public static final String LEXICALLY_SUGGEST_LOGICALLY_DEFINE_ISSUE = Namespaces.OQUO_NS + "LexicallySuggestLogicallyDefineIssue";
 	
 	/** The Constant SYSTEMATIC_NAMING_ISSUE. */
-	public static final String SYSTEMATIC_NAMING_ISSUE = "https://purl.archive.org/oquo#SystematicNamingIssue";
+	public static final String SYSTEMATIC_NAMING_ISSUE = Namespaces.OQUO_NS + "SystematicNamingIssue";
 }
